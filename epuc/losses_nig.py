23766bce57a7ae17483d0d87820139d673de0827
mkjuergens <mira.kjuergens@gmail.com>
2023-11-13 16:35:59 +0100

    add evidence based regulaization for outer expectation minimization

--- epuc/losses_nig.py
+++ epuc/losses_nig.py
@@ -20,7 +20,9 @@ class NegativeLogLikelihoodLoss(torch.nn.Module):
 
 
 class outer_loss_der(torch.nn.Module):
-    def __init__(self, lambda_reg: float, epsilon: float = 0.0001):
+    """outer expectation minimisation for the NIG-prior case.
+    """
+    def __init__(self, lambda_reg: float, type_reg: str = "evidence", epsilon: float = 0.0001):
         super().__init__()
         self.lambda_reg = lambda_reg
         self.epsilon = epsilon
@@ -43,28 +45,38 @@ class inner_loss_der(torch.nn.Module):
     uncertainty as well as predictions, as described in Amini et al.
     """
 
-    def __init__(self, lambda_reg: float, epsilon: float = 0.0001):
+    def __init__(
+        self, lambda_reg: float, reg_type: str = "kl", epsilon: float = 0.0001
+    ):
         """
         Parameters
         ----------
         lambda_reg : float
            weight factor of regulaization
+        reg_type : string
+            type of regularization used. Must be in {"kl", "evidence"}
         epsilon : float, optional
             _description_, by default 0.0001
         """
         super().__init__()
         self.lambda_reg = lambda_reg
         self.epsilon = epsilon
+        self.reg_type = reg_type
 
     def forward(self, params_nig: list, y: torch.tensor):
         if self.lambda_reg > 0.0:
-            sum_kl_div = sum_kl_divergence_nig(params_nig, epsilon=self.epsilon)
+            if self.reg_type == "kl":
+                reg = sum_kl_divergence_nig(params_nig, epsilon=self.epsilon)
+            elif self.reg_type == "evidence":
+                reg = evidence_regulizer(params_nig, y)
+            else:
+                raise TypeError
         else:
-            sum_kl_div = 0.0
+            reg = 0.0
 
         inner_loss = inner_nig_loss(params_nig, y)
 
-        return inner_loss + self.lambda_reg * sum_kl_div
+        return inner_loss + self.lambda_reg * reg
 
 
 def inner_nig_loss(params: list, y: torch.tensor):
@@ -100,6 +112,22 @@ def inner_nig_loss(params: list, y: torch.tensor):
 
 
 def outer_nig_loss(params: list, y: torch.tensor):
+    """secodnary loss function using "outer expectaiton minimisation" and negative log likelihood
+    as primary loss, assuming normally distributed data and a normal inverse gamma (NIG) distribtued
+    prior distribution.
+
+    Parameters
+    ----------
+    params : list
+        list of (tensors of) parameters of the NIG distribution per instance: [gamma, nu, alpha, beta]
+    y : torch.tensor
+        tensor of target values
+
+    Returns
+    -------
+    float
+        loss
+    """
     gamma, nu, alpha, beta = params
 
     loss = -0.5 * (
@@ -111,3 +139,28 @@ def outer_nig_loss(params: list, y: torch.tensor):
     )
 
     return loss.sum()
+
+
+def evidence_regulizer(params, y):
+    """evindece bases regularization as proposed in Amini et al.
+
+    Parameters
+    ----------
+    params : list
+        parameters of the NIG distribution as predicted by the model.
+    y : torch.tensor
+        tensor of target valeus
+
+    Returns
+    -------
+    float
+        regularization loss
+    """
+
+    gamma, nu, alpha = params[:-1]
+
+    abs_dif = torch.abs(y - gamma)
+
+    reg = abs_dif * (2 * nu + alpha)
+
+    return reg.sum()
