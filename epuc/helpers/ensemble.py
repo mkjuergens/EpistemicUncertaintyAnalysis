4f8a1921f9574480879e94854d682fbbdf69af2b
mkjuergens <mira.kjuergens@gmail.com>
2023-12-13 00:05:06 +0100

    update ensemble predictions, add GaussianEnsemble

--- epuc/helpers/ensemble.py
+++ epuc/helpers/ensemble.py
@@ -19,7 +19,7 @@ class Ensemble:
         self.model_config = model_config
         self.ensemble_size = ensemble_size
         self.models = [create_model(model_config) for _ in range(ensemble_size)]
-
+        self.losses = []
     def train(
         self,
         dataset,
@@ -42,15 +42,16 @@ class Ensemble:
             data_loader = torch.utils.data.DataLoader(
                 dataset_train, batch_size=train_params["batch_size"], shuffle=True
             )
-            train_model(
+            model, loss = train_model(
                 model,
                 data_loader,
                 criterion=train_params["loss"],
                 n_epochs=train_params["n_epochs"],
                 optim=train_params["optim"],
-                return_loss=False,
+                return_loss=True,
                 **train_params["optim_kwargs"]
             )
+            self.losses.append(loss)
 
     def predict(self, x):
         """
@@ -72,9 +73,8 @@ class Ensemble:
         elif isinstance(preds[0], (list, tuple)):
             preds_out = torch.zeros(x.shape[0], len(preds), len(preds[0]))
             for pred in range(len(preds[0])):
-                preds_out[:, :, pred] = torch.stack(
-                    [preds[i][pred] for i in range(len(preds))]
-                ).view(x.shape[0], len(preds))
+                for ens in range(len(preds)):
+                    preds_out[:, ens, pred] = preds[ens][pred].squeeze()
         else:
             raise TypeError("unsupported prediction type {}".format(type(preds[0])))
         return preds_out
@@ -83,3 +83,27 @@ class Ensemble:
         preds = self.predict(x)
         # predict mean over ensemble members
         return preds.mean(dim=1)
+    
+class GaussianEnsemble(Ensemble):
+
+    def __init__(self, model_config: dict, ensemble_size: int):
+        super().__init__(model_config, ensemble_size)
+
+    def predict_mean_var(self, x):
+
+        preds = self.predict(x) # of shape (n_instances, n_ensemble, 2)
+        pred_mean = preds[:, :, 0].mean(dim=1)
+        # use formula for variance of gauusian mixture model
+        preds_var = (preds[:, :, 1]**2 + preds[:, :, 0] ** 2).mean(dim=1) - pred_mean ** 2
+        preds_std = torch.sqrt(preds_var)
+        return pred_mean, preds_std
+
+
+if __name__ == "__main__":
+    from epuc.configs import model_config, data_config, train_config
+    ensemble = Ensemble(model_config["Normal"], ensemble_size=2)
+    data_params = data_config["SineRegression"]
+    train_params = train_config["Normal"]
+    ensemble.train(SineRegressionDataset, data_params, train_params)
+    x = torch.linspace(0, 1, 100).view(-1, 1)
+    preds = ensemble.predict_mean(x)
