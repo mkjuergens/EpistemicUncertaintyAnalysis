e1333d9df91404268b579dc0ffc1747772c5ebd9
mkjuergens <mira.kjuergens@gmail.com>
2024-01-02 21:38:06 +0100

    add Gaussian and NIG Ensemble

--- epuc/helpers/ensemble.py
+++ epuc/helpers/ensemble.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm import tqdm
 from epuc.helpers.train import train_model
 from epuc.models import create_model
 from epuc.datasets import SineRegressionDataset
@@ -20,6 +21,7 @@ class Ensemble:
         self.ensemble_size = ensemble_size
         self.models = [create_model(model_config) for _ in range(ensemble_size)]
         self.losses = []
+
     def train(
         self,
         dataset,
@@ -37,7 +39,7 @@ class Ensemble:
         train_params : dict
             dictionary containing the parameters for training
         """
-        for model in self.models:
+        for model in tqdm(self.models):
             dataset_train = dataset(**data_params)
             data_loader = torch.utils.data.DataLoader(
                 dataset_train, batch_size=train_params["batch_size"], shuffle=True
@@ -83,24 +85,91 @@ class Ensemble:
         preds = self.predict(x)
         # predict mean over ensemble members
         return preds.mean(dim=1)
-    
+
+
 class GaussianEnsemble(Ensemble):
+    """Ensmeble of models which predict a Gaussian distribution. The mean and variance of the ensemble
+    are computed based on assuming a Gaussian Mixture model.
+    """
+
+    def __init__(self, model_config: dict, ensemble_size: int):
+        """
+        Parameters
+        ----------
+        model_config : dict
+            dictionary of model parameters
+        ensemble_size : int
+            number of ensemble members
+        """
+        super().__init__(model_config, ensemble_size)
+
+    def predict_mean_var(self, x):
+        """
+        Parameters
+        ----------
+        x : torch.tensor
+            tensor of instances
+
+        Returns
+        -------
+        torch.tensor, torch.tensor
+            mean and variance of the ensemble
+        """
+
+        preds = self.predict(x)  # of shape (n_instances, n_ensemble, 2)
+        pred_mean = preds[:, :, 0].mean(dim=1)
+        # use formula for variance of gauusian mixture model
+        preds_var = (preds[:, :, 1] ** 2 + preds[:, :, 0] ** 2).mean(
+            dim=1
+        ) - pred_mean**2
+        preds_std = torch.sqrt(preds_var)
+        return pred_mean, preds_std
+    
+
+class NIGEnsemble(Ensemble):
+    """Ensemble of models predicting the parameters of a Normal-Inverse-Gamma distribution.
+    """
 
     def __init__(self, model_config: dict, ensemble_size: int):
         super().__init__(model_config, ensemble_size)
 
-    def predict_mean_var(self, x):
+    def predict_normal_params(self, x):
+        """predict parameters of a normal distribution based on the ensemble predictions.
+        Here, we take the mean of the ensemble predictions as the mean of the NIG parameters
 
-        preds = self.predict(x) # of shape (n_instances, n_ensemble, 2)
-        pred_mean = preds[:, :, 0].mean(dim=1)
-        # use formula for variance of gauusian mixture model
-        preds_var = (preds[:, :, 1]**2 + preds[:, :, 0] ** 2).mean(dim=1) - pred_mean ** 2
-        preds_std = torch.sqrt(preds_var)
-        return pred_mean, preds_std
+        Parameters
+        ----------
+        x : torch.tensor
+            tensor of instances
+
+        Returns
+        -------
+        mu, sigma
+            mean and variance of the predicted normal distribution
+        """
+        
+        # predict mean over ensemble members
+        preds = self.predict_mean(x)
+        gamma, nu, alpha, beta = preds[:, 0], preds[:, 1], preds[:, 2], preds[:, 3]
+        mu = gamma
+        sigma = torch.sqrt(beta / (alpha - 1))
+
+        return mu, sigma
+    
+    def predict_uc(self, x):
+
+        preds = self.predict_mean(x)
+        gamma, nu, alpha, beta = preds[:, 0], preds[:, 1], preds[:, 2], preds[:, 3]
+        ep_uc = beta / (nu * (alpha - 1))
+        al_uc = ep_uc * nu
+
+        return ep_uc, al_uc
+
 
 
 if __name__ == "__main__":
     from epuc.configs import model_config, data_config, train_config
+
     ensemble = Ensemble(model_config["Normal"], ensemble_size=2)
     data_params = data_config["SineRegression"]
     train_params = train_config["Normal"]
