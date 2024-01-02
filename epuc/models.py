d70bc900bd34ef9a41d2f32aedc326ad6ce35944
mkjuergens <mira.kjuergens@gmail.com>
2024-01-02 21:38:50 +0100

    add adjustable depth for the models

--- epuc/models.py
+++ epuc/models.py
@@ -7,71 +7,74 @@ class PredictorModel(nn.Module):
     Simple neural network for predicting the probability of a binary event.
     """
 
-    def __init__(self, hidden_dim: int = 10, output_dim: int = 1, use_relu: bool = False,
-                 use_softplus: bool = True) -> None:
+    def __init__(
+        self,
+        hidden_dim: int = 10,
+        output_dim: int = 1,
+        n_hidden_layers: int = 1,
+        use_softplus: bool = True,
+    ) -> None:
         super().__init__()
-        self.hidden_dim = hidden_dim
 
-        self.fc1 = nn.Linear(1, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, output_dim)
-        self.relu = nn.ReLU()
-        self.use_relu = use_relu
-        self.softplus = nn.Softplus()
-        self.use_softplus = use_softplus
-        self.sigmoid = nn.Sigmoid() # binary setting: probability for class 1
+        self.hidden_dim = hidden_dim
+        self.n_hidden_layers = n_hidden_layers
+        self.layers = [nn.Linear(1, hidden_dim), nn.Softplus() if use_softplus else nn.ReLU()]
+        for _ in range(n_hidden_layers - 1):
+            self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+            self.layers.append(nn.Softplus() if use_softplus else nn.ReLU())
+
+        self.layers.append(nn.Linear(hidden_dim, output_dim))
+        self.layers.append(nn.Sigmoid())
+
+        self.model = nn.Sequential(*self.layers)
 
     def forward(self, x):
-        if self.use_relu:
-            x = self.relu(self.fc1(x))
-            x = self.relu(self.fc2(x))
-        elif self.use_softplus:
-            x = self.softplus(self.fc1(x))
-            x = self.softplus(self.fc2(x))
-        else:
-            x = self.fc1(x)
-            x = self.fc2(x)
-        x = self.sigmoid(self.fc3(x))
-
+        x = self.model(x)
         return x
 
-    
+
 class RegressorModel(nn.Module):
     """
-    Simple neural network for predicting the mean and variance of a normally distributed 
+    Simple neural network for predicting the mean and variance of a normally distributed
     random variable, in dependence of the input.
     """
 
-    def __init__(self, hidden_dim: int = 10, use_softplus: bool = True, output_dim: int = 1):
+    def __init__(
+        self, hidden_dim: int = 10, use_softplus: bool = True, n_hidden_layers: int = 1,
+         output_dim: int = 1
+    ):
         super().__init__()
         self.hidden_dim = hidden_dim
-        self.fc1 = nn.Linear(1, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.n_hidden_layers = n_hidden_layers
+
+        self.layers = [nn.Linear(1, hidden_dim), nn.Softplus() if use_softplus else nn.ReLU()]
+        for _ in range(n_hidden_layers - 1):
+            self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+            self.layers.append(nn.Softplus() if use_softplus else nn.ReLU())
+
         self.fc_mu = nn.Linear(hidden_dim, output_dim)
         self.fc_sigma = nn.Linear(hidden_dim, output_dim)
-        self.softplus = nn.Softplus()
-        self.use_softplus = use_softplus
 
-    def forward(self, x):
-        if self.use_softplus:
-            x = self.softplus(self.fc1(x))
-            x = self.softplus(self.fc2(x))
-        else:
-            x = self.fc1(x)
-            x = self.fc2(x)
+        self.model = nn.Sequential(*self.layers)
         
+    def forward(self, x):
+        
+        x = self.model(x)
+
         mu = self.fc_mu(x)
         sigma = torch.exp(self.fc_sigma(x))
 
         return mu, sigma
-    
+
 
 class BetaNN(nn.Module):
     """
     Simple neural network for predicting the parameters of (uni- or multivariate) a Beta distribution.
     """
 
-    def __init__(self,hidden_dim: int = 10, use_softplus: bool = True, output_dim: int = 1):
+    def __init__(
+        self, hidden_dim: int = 10, use_softplus: bool = True, output_dim: int = 1
+    ):
         super().__init__()
         self.fc1 = nn.Linear(1, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
@@ -92,7 +95,7 @@ class BetaNN(nn.Module):
         beta = torch.exp(self.fc_beta(x))
 
         return alpha, beta
-    
+
 
 class NIGNN(nn.Module):
     """
@@ -100,7 +103,12 @@ class NIGNN(nn.Module):
     normal inverse gamma distribution.
     """
 
-    def __init__(self, hidden_dim: int = 10, use_softplus: bool = True, output_dim: int = 1,):
+    def __init__(
+        self,
+        hidden_dim: int = 10,
+        use_softplus: bool = True,
+        output_dim: int = 1,
+    ):
         super().__init__()
         self.hidden_dim = hidden_dim
         self.fc_1 = nn.Linear(1, hidden_dim)
@@ -122,13 +130,14 @@ class NIGNN(nn.Module):
         else:
             x = self.fc_1(x)
             x = self.fc_2(x)
-        
+
         gamma = self.fc_gamma(x)
         nu = self.softplus(self.fc_nu(x))
         alpha = self.softplus(self.fc_alpha(x)) + 1
         beta = self.softplus(self.fc_beta(x))
 
         return gamma, nu, alpha, beta
+
 
 def create_model(config):
     """
@@ -140,5 +149,8 @@ def create_model(config):
 
 if __name__ == "__main__":
     from epuc.configs import model_config
+
     model = create_model(model_config["Normal"])
     print(model)
+    x = torch.randn(10, 1)
+    print(model(x))
